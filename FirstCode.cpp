41d347b8eff95d276dc54b2a469079f7060d01a0
nitin pradhan <nitinpradhan48@gmail.com>
2026-05-19 09:30:58 +0530

    FirstCode

---
+++ FirstCode.cpp
@@ -0,0 +1,26 @@
+// This is my first code for learning c++ with DSA -- 19th May 2026 //
+
+// Include the iostream library which provides input/output functionality (cin, cout)
+#include <iostream>
+
+// Use the standard namespace to avoid writing std:: before cout, cin, etc.
+using namespace std;
+
+// Main function - the entry point of every C++ program
+// int indicates this function returns an integer value
+int main()
+{
+    // cout is used to print output to the console
+    // << is the insertion operator that sends data to cout
+    // "hello world" is a string literal that will be printed
+    // "\n" creates a new line after the text
+    cout <<"hello world" << "\n";
+    // return 0 indicates the program executed successfully
+    // 0 means no errors, non-zero values indicate errors
+    return 0;
+}
+
+// This is my first code for learning c++ with DSA -- 19th May 2026
+
+// To compile and exe this code run this cmd 
+// g++ FirstCode.cpp && ./a.out
