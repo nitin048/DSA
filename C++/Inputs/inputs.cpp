c727081cc148a68b9e1dcd8d727ef65ddfaa82fc
nitin pradhan <nitinpradhan48@gmail.com>
2026-05-19 17:18:06 +0530

    inputs

---
+++ C++/Inputs/inputs.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout << "What is your age is ?" << endl;
+    cin >> age;
+    cout << "Your age is : "<< age << endl;
+    return 0;
+}
+
+//g++ inputs.cpp && ./a.out
